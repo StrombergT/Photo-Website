eb15555a711a5831e954386637c0b7f653b3a841
StrombergT <tpr.stromberg@gmail.com>
2024-12-17 13:17:02 +0100

    Refactor homepage layout: moved Photoshoot component inside tile and updated structure
    - Moved the Photoshoot component inside each tile for proper display of images.
    - Updated the layout for better responsiveness with flexbox.
    - Reorganized code structure for a more modular design.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-/* import GalleryComponent from "../components/layout/home/GalleryComponent";
-
-export default function Home() {
-  return (
-    <div>
-      <GalleryComponent />
-    </div>
-  );
-}
- */
 import { HorizontalScroller } from "../components/HorizontalScroller";
 import { Tile } from "../components/Tile";
 import { Photoshoot } from "../components/PhotoShoot";
@@ -23,27 +13,31 @@ export default function Home() {
   return (
     <main style={{ counterReset: "photoshoot-counter" }}>
       <HorizontalScroller>
-        {images.map((imageSrc, index) => (
-          <Tile key={index}>
-            <div className="mx-7 py-12 xl:m-0 xl:w-[60vw] xl:max-w-[900px] xl:flex xl:items-center xl:justify-items-center xl:p-32">
-              <div>
-                <div className="uppercase tracking-widest text-sm mb-12 xl:mb-20 text-black">
-                  John Doe Photography
-                </div>
-                <h1 className="text-black font-serif mb-12 text-5xl xl:text-8xl tracking-tight">
-                  Fångar ögonblicken som varar för evigt.
-                </h1>
-                <div className="leading-loose prose max-w-none text-black">
-                  Fotografering handlar inte bara om bilder – det är att fånga
-                  din berättelse och förvandla den till något unikt och
-                  minnesvärt. Låt mig hjälpa dig att berätta din historia genom
-                  min lins.
-                </div>
+        <Tile>
+          <div className="mx-7 py-12 xl:m-0 xl:w-[60vw] xl:max-w-[900px] xl:flex xl:items-center xl:justify-between xl:p-32">
+            <div>
+              <div className="uppercase tracking-widest text-sm mb-12 xl:mb-20 text-black">
+                John Doe Photography
+              </div>
+              <h1 className="text-black font-serif mb-12 text-5xl xl:text-8xl tracking-tight">
+                Fångar ögonblicken som varar för evigt.
+              </h1>
+              <div className="leading-loose prose max-w-none text-black">
+                Fotografering handlar inte bara om bilder – det är att fånga din
+                berättelse och förvandla den till något unikt och minnesvärt.
+                Låt mig hjälpa dig att berätta din historia genom min lins.
               </div>
             </div>
-            <Photoshoot total={images.length} />
-          </Tile>
-        ))}
+          </div>
+
+          <div className="flex flex-col xl:flex-row overflow-x-auto ">
+            {images.map((imageSrc, index) => (
+              <Tile key={index}>
+                <Photoshoot total={images.length} imageSrc={imageSrc} />
+              </Tile>
+            ))}
+          </div>
+        </Tile>
       </HorizontalScroller>
     </main>
   );
