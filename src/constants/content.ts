92b152fb0492fe7fb17c83d28589f12a43dab554
StrombergT <tpr.stromberg@gmail.com>
2024-12-18 20:08:10 +0100

    add images and dummy data for now

---
+++ src/constants/content.ts
@@ -0,0 +1,57 @@
+export const content = {
+  wedding: {
+    title: "Bröllop",
+    items: [
+      {
+        image: "/img/work/wedding.jpg",
+        slug: "wedding",
+      },
+    ],
+  },
+  portrait: {
+    title: "Porträtt",
+    items: [
+      {
+        image: "/img/work/portrait.jpg",
+        slug: "portrait",
+      },
+    ],
+  },
+  family: {
+    title: "Familjefotografering",
+    items: [
+      {
+        image: "/img/work/family_photo.jpg",
+        slug: "family",
+      },
+    ],
+  },
+};
+
+export const aboutMyWork = {
+  text: `Doloremque, quidem. Doloremque, quidem. Possimus, quidem. Quae, quidem. Doloremque, quidem. 
+  Doloremque, quidem. Possimus, quidem. Quae, quidem. Doloremque, quidem. Doloremque, q
+   Doloremque, quidem. Possimus, quidem. Quae, quidem. Doloremque, quidem. Doloremque, q
+`,
+};
+
+export const weddingImages = [
+  "/img/work/wedding.jpg",
+  "/img/work/wedding.jpg",
+  "/img/work/wedding.jpg",
+  "/img/work/wedding.jpg",
+];
+
+export const portraitImages = [
+  "/img/work/portrait.jpg",
+  "/img/work/portrait.jpg",
+  "/img/work/portrait.jpg",
+  "/img/work/portrait.jpg",
+];
+
+export const familyImages = [
+  "/img/work/family_photo.jpg",
+  "/img/work/family_photo.jpg",
+  "/img/work/family_photo.jpg",
+  "/img/work/family_photo.jpg",
+];
